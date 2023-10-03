a582caf565c753cb69b1febca85160c4f1b3494e
Trilochan Choudhary <145929988+triloc8084@users.noreply.github.com>
2023-10-03 19:32:10 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,30 @@
+"""num=int(input("k: "))
+sum1=0
+a=0
+b=1
+c=0
+s=0
+p=0
+while(c<=num):
+    print(c)
+    a=b
+    b=c
+    if(p%2==0):
+        s+=c
+    p=p+1
+    c=a+b
+
+print("sum:",s)"""
+
+"""n=int(input(" "))
+y=(n**(1/3))
+print("{0:.3f}".format(y))"""
+
+n=int(input("enter: "))
+tup=list()
+for i in range(1,n+1):
+    n=int(input("enterabc: "))
+    n=n**3
+    tuple=tup.append(n)
+    print(tuple)
+
